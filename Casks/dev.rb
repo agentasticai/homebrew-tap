5c18e1ad50eeb331ace335566907e2238d49ddb6
Adel Ahmadyan <ahmadyan@gmail.com>
2026-04-22 23:11:18 -0700

    Update Agentastic.dev to 0.7.2

--- Casks/dev.rb
+++ Casks/dev.rb
@@ -1,6 +1,6 @@
 cask "dev" do
-  version "0.7.1"
-  sha256 "d73e497e1778780197af414580df6429501d379b73faeca8ae8dc8b6317022f0"
+  version "0.7.2"
+  sha256 "145d31a109378aa9c9aa6b1df988c7f95dfc6a37ed1ab8df4e2a39a85633d8a6"
 
   url "https://releases.agentastic.ai/agentasticdev/Agentastic.dev-#{version}.dmg"
   name "Agentastic.dev"
