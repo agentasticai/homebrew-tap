da17baa35da9439e7a0e1c74da865661b73e1937
Adel Ahmadyan <ahmadyan@gmail.com>
2026-07-11 13:37:06 -0700

    Update Agentastic.dev to 0.8.7

--- Casks/dev.rb
+++ Casks/dev.rb
@@ -1,13 +1,14 @@
 cask "dev" do
-  version "0.8.6"
-  sha256 "1691550120a5f1b58be7003091eb5b94126baac70d829d1d557cd56d96739a92"
+  version "0.8.7"
+  sha256 "08581441e41efde42e797141e82495c220e34fec92bf1977538490e42aeaa551"
 
-  url "https://releases.agentastic.ai/agentasticdev/Agentastic.dev-#{version}.dmg"
+  url "https://releases.agentastic.ai/agentasticdev/Agentastic.dev-#{version}.dmg",
+      verified: "releases.agentastic.ai/agentasticdev/"
   name "Agentastic.dev"
   desc "Native macOS code editor"
   homepage "https://www.agentastic.dev"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "Agentastic.dev.app"
 
